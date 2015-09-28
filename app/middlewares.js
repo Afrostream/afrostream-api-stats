2df573b1b991e977fe8158e45ae9569be08fdf2b
Marc Dassonneville <marc.dassonneville@gmail.com>
2015-09-28 09:58:19 +0200

    configuration of express middlewares inside app/middlewares

---
+++ app/middlewares.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var compression = require('compression')
+  , bodyParser = require('body-parser');
+
+var config = rootRequire('/config');
+
+/**
+ * setting up express app middlewares
+ * @param app
+ */
+module.exports = function (app) {
+  // first middleware : are we overloaded ?
+  app.use(rootRequire('/lib/middleware-toobusy')());
+
+  // req.error error handler
+  app.use(rootRequire('/lib/middleware-error')());
+  app.use(rootRequire('/lib/middleware-catchall')());
+
+  // parsing POST data
+  app.use(compression());
+  app.use(bodyParser.urlencoded({extended: false}));
+  app.use(bodyParser.json());
+
+  //
+  app.use(rootRequire('/lib/middleware-allowcrossdomain')({allowOrigin: config.allowOrigin}));
+  app.use(rootRequire('/lib/middleware-jsoncontenttype')());
+};
