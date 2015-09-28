daf0874b0aa38aadbd2be46f0fec9d10751dc2c8
Marc Dassonneville <marc.dassonneville@gmail.com>
2015-09-28 12:50:18 +0200

    api: /api/v1/event/:id is limited to env test & development

--- app/api/v1/event/index.js
+++ app/api/v1/event/index.js
@@ -3,8 +3,9 @@
 var router = require('express').Router();
 var controller = require('./event.controller.js');
 var validator = require('./event.validator.js');
+var env = rootRequire('lib/middleware-env');
 
 router.post('/', validator(), controller.create);
-router.get('/:id', controller.show);
+router.get('/:id', env({allowed:['development', 'test']}), controller.show);
 
 module.exports = router;
