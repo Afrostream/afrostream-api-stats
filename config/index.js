dc0ef481c7ee67929c0e7e773cb59e11d2d203d0
Marc Dassonneville <marc.dassonneville@gmail.com>
2015-09-28 12:59:56 +0200

    npm start dev is now using nodemon instead of pm2

--- config/index.js
+++ config/index.js
@@ -5,6 +5,7 @@ var _ = require('lodash');
 var all = {
   port: process.env.PORT || 3003,
   knex: {
+    debug: true,
     client: 'pg',
     connection: process.env.DATABASE_URL || 'postgres://postgres:root@localhost:5432/cdnselector'
   }
