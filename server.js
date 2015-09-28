d40373fa9466508aed7adc549d1390175f7e8931
Marc Dassonneville <marc.dassonneville@gmail.com>
2015-09-28 12:00:01 +0200

    suppression de pm2, utilisation d'un module cluster plus simple (fait par flipboard).

--- server.js
+++ server.js
@@ -1,39 +1,10 @@
 'use strict';
 
-var pm2 = require('pm2');
+var cluster = require('express-cluster');
 
-/*
-var MACHINE_NAME = process.env.WEB_MACHINE_NAME || require('os').hostname() || 'apistats';
-var PRIVATE_KEY = process.env.KEY_METRICS_PRIVATE_KEY || 'qigcd3tiue2el5k';   // Keymetrics Private key
-var PUBLIC_KEY = process.env.KEY_METRICS_PUBLIC_KEY || 'ns4xyu6prfqcarv';     // Keymetrics Public  key
-*/
-var instances = process.env.WEB_CONCURRENCY || -1; // Set by Heroku or -1 to scale to max cpu core -1
-var maxMemory = process.env.WEB_MEMORY || 512;
+var clusterConf = {count: process.env.WEB_CONCURRENCY || -1, verbose: true};
 
-var conf = {
-  instances: instances,
-  max_memory_restart: maxMemory + 'M',   // Auto restart if process taking more than XXmo
-  post_update: ['npm install'],          // Commands to execute once we do a pull from Keymetrics
-  apps: [
-    {
-      name: "afrostream-api-stats",
-      script: "./app/index.js",
-      exec_mode: "cluster",
-      autorestart: true
-    }
-  ]
-};
-
-pm2.connect(function () {
-  pm2.start(conf, function (err) {
-    if (err) {
-      return console.error('Error while launching applications', err.stack || err);
-    }
-    console.log('PM2 and application has been succesfully started');
-    /*
-    if (process.env.NODE_ENV === 'production') {
-      pm2.interact(PRIVATE_KEY, PUBLIC_KEY, MACHINE_NAME, function () { });
-    }
-    */
-  });
-});
+cluster(function (worker) {
+  console.log('worker '+worker.id+' is up');
+  return require('./app/index.js');
+}, clusterConf);
