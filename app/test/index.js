3dd2dda9d54ebd51d0fe2c01a50f44587c67c517
Marc Dassonneville <marc.dassonneville@gmail.com>
2015-10-15 17:41:19 +0200

    routes /test/headers & /test/request-infos dumping headers & ip info to check the X-Forwarded-For on heroku

--- app/test/index.js
+++ app/test/index.js
@@ -6,13 +6,30 @@ var random = require('./random.js');
 
 var readFile = require('fs').readFile;
 
-router.get('/insert', random.insertRandomData);
-router.get('/stats.html', function (req, res) {
-  readFile(__dirname + '/stats.html', function (err, text) {
-    res.set('Content-Type', 'text/html');
-    res.send(text);
-  });
+/*
+ router.get('/insert', random.insertRandomData);
+ router.get('/stats.html', function (req, res) {
+ readFile(__dirname + '/stats.html', function (err, text) {
+ res.set('Content-Type', 'text/html');
+ res.send(text);
+ });
+ });
+ router.get('/stats', random.getStats);
+*/
+
+router.get('/headers', function (req, res) {
+  res.set('Content-Type', 'text/plain');
+  res.send(JSON.stringify(req.headers));
 });
-router.get('/stats', random.getStats);
+
+router.get('/request-infos', function (req, res) {
+  res.set('Content-Type', 'text/plain');
+  res.send(
+    'headers : ' + "\n" +
+    JSON.stringify(req.headers) + "\n" +
+    'req.ip : ' + req.ip + "\n" +
+    'req.ips : ' + JSON.stringify(req.ips) + "\n"
+  );
+});
 
 module.exports = router;
