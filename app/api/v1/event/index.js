3c65cce2b8aa5458a5aee7cc4034a7877dba0da7
Marc Dassonneville <marc.dassonneville@gmail.com>
2015-10-19 15:22:32 +0200

    bugfix fwd rabbit mq

--- app/api/v1/event/index.js
+++ app/api/v1/event/index.js
@@ -5,7 +5,7 @@ var controller = require('./event.controller.js');
 var validator = require('./event.validator.js');
 var env = rootRequire('lib/middleware-env');
 var config = rootRequire('config');
-var middlewareFwdToMq = config.fwdToMq ? rootRequire('lib/middleware-fwdtomq') : function (a, b, n) { return n(); };
+var middlewareFwdToMq = config.fwdToMq ? rootRequire('lib/middleware-fwdtomq')() : function (a, b, n) { return n(); };
 
 router.post('/', validator.middleware(), middlewareFwdToMq, controller.create);
 router.get('/:id', env({allowed:['development', 'test']}), controller.show);
